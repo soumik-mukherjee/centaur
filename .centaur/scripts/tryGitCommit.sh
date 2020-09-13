257789aadc66d4f3aa2e00c24c5d79750ef146cf
soumik-mukherjee <soumik@DESKTOP-C2U0OC2.localdomain>
2020-09-13 12:57:58 +0530

    centaur:

--- .centaur/scripts/tryGitCommit.sh
+++ .centaur/scripts/tryGitCommit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-git add .
+
 DIFF=$(git diff --raw)
 if [ -z "$DIFF" ] 
 then
-	echo "Diff found. Ready to commit"
-  git commit -m "centaur: $COMMIT_MESSAGE"
-  git push origin master   
+	echo "No changes to commit. Nothing to do"
 else
-	echo "No changes to commit"
+  echo "Diff found. Ready to commit"
+  git add .
+  git commit -m "centaur: $COMMIT_MESSAGE"
+  git push origin master
 fi
